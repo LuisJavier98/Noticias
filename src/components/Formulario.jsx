bfb3d5f4f01c9d410f2f229b553201bcd9feb018
Luis <luisjavier_2705@hotmail.com>
2023-02-08 08:37:19 -0500

    primer commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,35 @@
+import { FormControl, InputLabel, Select, MenuItem, Button, Box } from "@mui/material"
+import useNoticias from "../hooks/useNoticias"
+
+const CATEGORIAS = [
+  { value: 'general', label: 'General' },
+  { value: 'business', label: 'Negocios' },
+  { value: 'entertainment', label: 'Entretenimiento' },
+  { value: 'health', label: 'Salud' },
+  { value: 'science', label: 'Ciencia' },
+  { value: 'sports', label: 'Deportes' },
+  { value: 'technology', label: 'Tecnología' },
+]
+
+const Formulario = () => {
+
+  const { categoria, handleChangeCategoria } = useNoticias()
+  return (
+    <div>
+      <FormControl fullWidth >
+        <InputLabel>Categoria</InputLabel>
+        <Select
+          label='Categotia'
+          onChange={handleChangeCategoria}
+          value={categoria}
+        >
+          {CATEGORIAS.map(categoria => (
+            <MenuItem key={categoria.value} value={categoria.value}>{categoria.label} </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  )
+}
+
+export default Formulario
---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,59 @@
+import axios from "axios"
+import { createContext, useEffect, useState } from "react"
+
+export const NoticiasContext = createContext()
+
+const NoticiasProvider = ({ children }) => {
+  const [categoria, setcategoria] = useState('general')
+  const [noticias, setnoticias] = useState([])
+  const [pagina, setpagina] = useState(1)
+  const [totalNoticias, settotalNoticias] = useState(0)
+  const handleChangeCategoria = e => {
+    setcategoria(e.target.value)
+  }
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apikey=${import.meta.env.VITE_API_KEY}`
+      const { data } = await axios(url)
+      console.log(data)
+      setnoticias(data.articles)
+      settotalNoticias(data.totalResults)
+      setpagina(1)
+    }
+    consultarAPI()
+  }, [categoria])
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&page=${pagina}&apikey=${import.meta.env.VITE_API_KEY}`
+      const { data } = await axios(url)
+      console.log(data)
+      setnoticias(data.articles)
+      settotalNoticias(data.totalResults)
+    }
+    consultarAPI()
+  }, [pagina])
+
+  const handleChangePagina = (e, valor) => {
+    setpagina(valor)
+  }
+
+
+  return (
+    <NoticiasContext.Provider
+      value={{
+        categoria,
+        handleChangeCategoria,
+        noticias,
+        totalNoticias,
+        handleChangePagina,
+        pagina
+      }}
+    >
+      {children}
+    </NoticiasContext.Provider>
+  )
+}
+
+export default NoticiasProvider
